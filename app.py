aa3c07eac68e2bd38735c3976c1b70d9b5149436
Guilherme Lanzoni Teixeira <guilhermelanzoni-ieg@instituto.ad>
2024-09-23 00:11:00 -0300

    alteração no estilo

--- app.py
+++ app.py
@@ -4,7 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '''
+    mensagem_1 = "Hello, world!"
+    mensagem_2 = "I love DevOps!"
+    style = '''
+        body {
+            background-color: #1C1C1C;
+        }
+        h1 {
+            font-family: "Inter", sans-serif;
+            font-weight: 800;
+            color: #8F2FDB;
+            font-size:500%%;
+        }
+    '''
+    return f'''
         <html>
         <head>
             <title>Exercício DevOps</title>
@@ -12,21 +25,13 @@ def hello_world():
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
             <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
             <style>
-                body {
-                    background-color: #1C1C1C;
-                }
-                h1 {
-                    font-family: "Inter", sans-serif;
-                    font-weight: 800;
-                    color: #8F2FDB;
-                    font-size:4%;
-                }
+                {style}
             </style>
         </head>
         <body>
             <div style="margin:5%;">
-                <h1>Hello, world!</h1>
-                <h1>I love DevOps!</h1>
+                <h1>`{mensagem_1.upper()}</h1>
+                <h1>{mensagem_2.upper()}</h1>
             </div>
         </body>
         </html>
